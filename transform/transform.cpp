4141948e50c880e631e14ba21c3d64d1662bef4c
Percy Maldonado Quispe <maldonadoq.percy@gmail.com>
2019-03-24 11:23:11 -0500

    Solar System Main

--- transform/transform.cpp
+++ transform/transform.cpp
@@ -15,6 +15,7 @@
 float wsize = 50;
 float msize = wsize/2;
 
+/*
 float teapot_angle  	= 0;
 float teapot_speed 		= 3;
 float teapot_translate  = 10;
@@ -35,6 +36,32 @@ float torus_angle  		= 0;
 float torus_speed 		= 3*teapot_speed;
 float torus_translate  	= 5;
 float torus_inner		= 0.3;
+*/
+
+float sun_anglec		= 0;
+float sun_speedc		= 1;
+float sun_radius		= 4;
+
+float earth_anglec		= 0;
+float earth_anglet		= 0;
+float earth_speedc		= 3*sun_speedc;
+float earth_speedt		= 0.02;
+float earth_radius		= 2;
+float earth_translate	= 10;
+
+float moon_anglec		= 0;
+float moon_anglet		= 0;
+float moon_speedc		= 1.5*sun_speedc;
+float moon_speedt		= 2*earth_speedt;
+float moon_radius		= 1;
+float moon_translate	= 4;
+
+float mars_anglec		= 0;
+float mars_anglet		= 0;
+float mars_speedc		= sun_speedc;
+float mars_speedt		= earth_speedt;
+float mars_radius		= 3;
+float mars_translate	= 18;
 
 void TPaint(){
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);	
@@ -44,16 +71,19 @@ void TPaint(){
 	// glOrtho(-25.0f, 25.0f, -25.0f, 25.0f, -25.0f, 25.0f);
 	glOrtho(-msize,msize,-msize,msize, -25.0f, 25.0f);
 
-
-	// glutSolidTeapot(10);
-	// TTranslate(teapot_angle, teapot_translate);
-	// TTranslateIdaVuelta(teapot_angle, teapot_translate,
-	//		circle_direction, circle_xpos, circle_radius);
+	/*
+	TTranslate(teapot_angle, teapot_translate);
+	TTranslateIdaVuelta(teapot_angle, teapot_translate, \
+			circle_direction, circle_xpos, circle_radius);
 	TTranslateComplex(teapot_angle, teapot_translate, \
 		circle_direction, circle_xpos, circle_radius, \
 		cube_angle, cube_translate, \
 		torus_angle, torus_translate, torus_inner);
-
+	*/
+	TSolarSystem(sun_anglec, sun_radius, \
+		earth_anglec, earth_anglet, earth_radius, earth_translate, \
+		moon_anglec, moon_anglet, moon_radius, moon_translate, \
+		mars_anglec, mars_anglet, mars_radius, mars_translate);
 
 	glutSwapBuffers();
 	glFlush();
@@ -108,6 +138,7 @@ void TTimer(int _t){
     glutPostRedisplay();
     glutTimerFunc(1000/60, TTimer, 0);
 
+    /*
     if(abs(circle_xpos) > circle_translate){
 		circle_direction = -1*circle_direction;
 	}
@@ -116,6 +147,18 @@ void TTimer(int _t){
 	teapot_angle += teapot_speed;
 	cube_angle   += cube_speed;
 	torus_angle  += torus_speed;
+	*/
+
+	sun_anglec   += sun_speedc;
+	
+	earth_anglec += earth_speedc;
+	earth_anglet += earth_speedt;
+
+	moon_anglec	 += moon_speedc;
+	moon_anglet  += moon_speedt;
+
+	mars_anglec += mars_speedc;
+	mars_anglet += mars_speedt;
 }
 
 // g++ figure.cpp -o figure.out -lglut -lGL
