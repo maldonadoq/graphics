d1e6c2a9082891816f562dcc9fe889419bdd32a3
Unknown <percy.maldonado@ucsp.edu.pe>
2019-06-13 21:00:25 -0500

    utils headers

---
+++ utils/draw.h
@@ -0,0 +1,25 @@
+#ifndef _DRAW_H_
+#define _DRAW_H_
+
+#include <GL/glut.h>
+
+void TAxis(float _x, float _y, float _z, float _size){
+	glBegin(GL_LINES);
+		// x
+		glColor3f(1,0,0);
+		glVertex3f(_x,_y,_z);
+		glVertex3f(_x+_size,_y,_z);
+
+		// y
+		glColor3f(0,1,0);
+		glVertex3f(_x,_y,_z);
+		glVertex3f(_x,_y+_size,_z);
+
+		// z
+		glColor3f(0,0,1);
+		glVertex3f(_x,_y,_z);
+		glVertex3f(_x,_y,_z+_size);
+	glEnd();
+}
+
+#endif
